81b77a7611d27c6c33daa74845c28de027efc346
Laura-ZP <laura.zp07@gmail.com>
2025-08-09 13:14:43 +0330

    add dob field

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,12 +6,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { jwtInterceptor } from './interceptor/jwt.interceptor';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
+    provideNativeDateAdapter(),
     provideHttpClient(withInterceptors([jwtInterceptor]))
   ]
 };
--- client/src/app/componens/account/register/register.component.ts
+++ client/src/app/componens/account/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AccountService } from '../../../services/account.service';
 import { FormBuilder, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppUser } from '../../../models/app-user.model';
@@ -6,27 +6,37 @@ import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-register',
   imports: [
     // RouterLink,
     ReactiveFormsModule, FormsModule,
-    MatButtonModule, MatInputModule, MatFormFieldModule       
+    MatButtonModule, MatInputModule, MatFormFieldModule, MatDatepickerModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   accountService = inject(AccountService);
   fB = inject(FormBuilder);
+
+  minDate = new Date();
+  maxDate = new Date();
+
+  ngOnInit(): void {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 99, 0, 1);
+    this.maxDate = new Date(currentYear - 6, 0, 1);
+  }
 
   registerFg = this.fB.group({
     emailCtrl: ['', [Validators.required, Validators.email]],
     userNameCtrl: [''],
     passwordCtrl: [''],
     confirmPasswordCtrl: [''],
-    ageCtrl: 0,
+    dateOfBirthCtrl: ['', [Validators.required]],
     genderCtrl: [''],
     cityCtrl: [''],
     countryCtrl: ['']
@@ -48,8 +58,8 @@ export class RegisterComponent {
     return this.registerFg.get('confirmPasswordCtrl') as FormControl;
   }
 
-  get AgeCtrl(): FormControl {
-    return this.registerFg.get('ageCtrl') as FormControl;
+  get DateOfBirthCtrl(): FormControl {
+    return this.registerFg.get('dateOfBirthCtrl') as FormControl;
   }
 
   get GenderCtrl(): FormControl {
@@ -65,22 +75,34 @@ export class RegisterComponent {
   }
 
   register(): void {
-    let user: AppUser = {
-       email: this.EmailCtrl.value,
-       userName: this.UserNameCtrl.value,
-       password: this.PasswordCtrl.value,
-       confirmPassword: this.ConfirmPasswordCtrl.value,
-       age: this.AgeCtrl.value,
-       gender: this.GenderCtrl.value,
-       city: this.CityCtrl.value,
-       country: this.CountryCtrl.value
+    const dob: string | undefined = this.getDateOnly(this.DateOfBirthCtrl.value);
+
+    if (this.PasswordCtrl.value === this.ConfirmPasswordCtrl.value) {
+      let user: AppUser = {
+        email: this.EmailCtrl.value,
+        userName: this.UserNameCtrl.value,
+        password: this.PasswordCtrl.value,
+        confirmPassword: this.ConfirmPasswordCtrl.value,
+        dateOfBirth: dob,
+        gender: this.GenderCtrl.value,
+        city: this.CityCtrl.value,
+        country: this.CountryCtrl.value
+      }
+
+      let registerResponse$ = this.accountService.register(user);
+
+      registerResponse$.subscribe({
+        next: (res) => console.log(res),
+        error: (err) => console.log(err.error)
+      });
     }
+  }
 
-    let registerResponse$ = this.accountService.register(user);
+  getDateOnly(dob: string | null): string | undefined {
+    if (!dob) return undefined;
 
-    registerResponse$.subscribe({
-      next: (res) => console.log(res),
-      error: (err) => console.log(err.error)
-    });
+    let theDob: Date = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes() - theDob.getTimezoneOffset())).toISOString().slice(0, 10);
+    // gets the first 10 chars from this date YYYY-MM-DDTHH:mm:ss.sssZ the output is YYYY-MM-DD
   }
 }
