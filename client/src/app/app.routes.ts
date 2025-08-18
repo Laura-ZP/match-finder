27ac9bf77fbf83db996288197eeaffa277cf1ec5
Laura-ZP <laura.zp07@gmail.com>
2025-08-18 20:49:18 +0330

    Add auth logged in guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,11 +7,12 @@ import { NavbarComponent } from './componens/navbar/navbar.component';
 import { MemberComponent } from './componens/member/member.component';
 import { NotFoundComponent } from './componens/not-found/not-found.component';
 import { authGuard } from './guards/auth.guard';
+import { authLoggedInGuard } from './guards/auth-logged-in.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'account/register', component: RegisterComponent },
-    { path: 'account/login', component: LoginComponent },
+    { path: 'account/register', component: RegisterComponent, canActivate: [authLoggedInGuard] },
+    { path: 'account/login', component: LoginComponent, canActivate: [authLoggedInGuard] },
     { path: 'footer', component: FooterComponent },
     { path: 'navbar', component: NavbarComponent },
     { path: 'members', component: MemberComponent, canActivate: [authGuard] },
