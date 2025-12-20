1a98545bfd247dde031c105921312992e462ac18
Laura-ZP <laura.zp07@gmail.com>
2025-12-20 13:56:36 +0330

    Used member card

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { MemberCardComponent } from '../member-card/member-card.component';
+import { MemberService } from '../../services/member.service';
+import { Member } from '../../models/member.model';
 
 @Component({
   selector: 'app-member-list',
-  imports: [],
+  imports: [
+    RouterLink,
+    MatCardModule, MatIconModule, MemberCardComponent
+  ],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.scss'
 })
 export class MemberListComponent {
+  memberService = inject(MemberService);
+  members: Member[] | undefined;
 
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(): void {
+    let allMember$: Observable<Member[]> = this.memberService.getAllMembers();
+
+    allMember$.subscribe({
+      next: (res) => {
+        this.members = res;
+        console.log(res);
+      }
+    });
+  }
 }
---
+++ client/src/app/services/member.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Member } from '../models/member.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberService {
+  http = inject(HttpClient);
+
+  private readonly _baseApiUrl: string = environment.apiUrl + 'api/member/';
+
+  getAllMembers(): Observable<Member[]> {
+
+    let members$: Observable<Member[]>
+     = this.http.get<Member[]>(this._baseApiUrl + 'get-all');      
+
+    return members$;
+  }
+
+  getByUserName(userNameInput: string): Observable<Member | undefined> {
+    return this.http.get<Member>(this._baseApiUrl + 'get-by-username/' + userNameInput);
+  }
+}
