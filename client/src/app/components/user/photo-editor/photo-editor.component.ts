48d6f949e9b3454fd6a3eef58a80eb6e497c6290
Laura-ZP <laura.zp07@gmail.com>
2025-12-22 21:55:20 +0330

    Designed user edit and photo editor component

--- client/src/app/components/user/photo-editor/photo-editor.component.ts
+++ client/src/app/components/user/photo-editor/photo-editor.component.ts
@@ -5,10 +5,20 @@ import { environment } from '../../../../environments/environment.development';
 import { FileUploader, FileUploadModule } from 'ng2-file-upload';
 import { AccountService } from '../../../services/account.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { Photo } from '../../../models/photo.model';
 
 @Component({
   selector: 'app-photo-editor',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule, MatInputModule, MatCardModule, MatIconModule, FileUploadModule, MatButtonModule
+  ],
   templateUrl: './photo-editor.component.html',
   styleUrl: './photo-editor.component.scss'
 })
@@ -22,8 +32,16 @@ export class PhotoEditorComponent {
   private accountService = inject(AccountService);
   private snackBar = inject(MatSnackBar);
 
+  constructor() {
+    this.loggedInUser = this.accountService.loggedInUserSig();
+  }
+
   ngOnInit(): void {
     this.initializeUploader();
+  }
+
+  fileOverBase(event: boolean): void {
+    this.hasBaseDropZoneOver = event;
   }
 
   initializeUploader(): void {
@@ -40,6 +58,13 @@ export class PhotoEditorComponent {
 
       this.uploader.onAfterAddingFile = (file) => {
         file.withCredentials = false;
+      }
+
+      this.uploader.onSuccessItem = (item, response, status, headers) => {
+        if (response) {
+          const photo: Photo = JSON.parse(response);
+          this.member?.photos.push(photo);
+        }
       }
     }
   }
